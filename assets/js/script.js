7bfcb415108f32c3af9d2c9eb14236b995d8f50f
Dohyun Moon <d6moon@uwaterloo.ca>
2024-07-12 19:26:07 -0400

    layout fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,32 @@ for (const image of images) {
         event.onerror = null;
     });
 }
+
+const navigation = document.getElementById("navigation");
+
+window.addEventListener("scroll", () => {
+    navigation.classList.add("hide");
+
+    if (window.scrollY == window.screenTop) {
+        navigation.classList.remove("hide");
+    }
+});
+
+navigation.addEventListener("mouseover", () => {
+    navigation.classList.remove("hide");
+});
+
+
+const mainButton = document.getElementById("main-button");
+
+mainButton.addEventListener("click", () => {
+    const img = document.getElementById("feeb");
+    
+    if (mainButton.dataset.evo == "feebas") {
+        img.src = "assets/images/milotic.png";
+        mainButton.dataset.evo = "milotic";
+    } else {
+        img.src = "assets/images/feebas.png";
+        mainButton.dataset.evo = "feebas";
+    }
+});
