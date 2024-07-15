97b5970e5f3a39b0ad28c4943a40a878a9e6f4e5
Dohyun Moon <d6moon@uwaterloo.ca>
2024-07-15 09:02:37 -0400

    animations added for experience section and profile picture added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-const images = document.getElementsByTagName("img");
-
-for (const image of images) {
-    image.addEventListener("error", function(event) {
-        event.target.src = "./assets/images/feebas.png";
-        event.onerror = null;
-    });
-}
-
 const navigation = document.getElementById("navigation");
 
 window.addEventListener("scroll", () => {
